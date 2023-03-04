53bf2d1a8cfe3b33410ee2f267cedd8a6368d3bc
Isabel <isabqm121@gmail.com>
2023-03-04 16:55:58 -0400

    Funcionalidad de Ingresar y Mostrar titulo con Clase Nota.js

---
+++ src/Nota.js
@@ -0,0 +1,9 @@
+class Nota{
+    constructor(titulo){
+        this.titulo=titulo;
+    }
+    GetTitulo(){
+        return this.titulo;
+    }
+}
+export default Nota;
--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,11 @@
+import Nota from "./Nota";
+
 const formularioNota = document.querySelector("#formularioNota");
 const tituloNota = document.querySelector("#tituloNota");
 const contenidoTituloNota = document.querySelector("#contenidoTituloNota");
 
 formularioNota.addEventListener("submit", event => {
     event.preventDefault();
-    contenidoTituloNota.innerHTML = "<p>" + "Título introducido: " + tituloNota.value + "</p>";
+    var notaNueva=new Nota(tituloNota.value);
+    contenidoTituloNota.innerHTML = "<p>" + "Título introducido: " + notaNueva.GetTitulo() + "</p>";
 })
