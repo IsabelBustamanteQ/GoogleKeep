4b9d8dafe2641eee48c9848aaecd2ac4a7a52785
Isabel <isabqm121@gmail.com>
2023-03-05 14:34:55 -0400

    Mostrar Lista de notas como funcion de la clase ListaNotas y Borrar Notas implementado

--- src/ListaNotas.js
+++ src/ListaNotas.js
@@ -6,7 +6,34 @@ class ListaNotas{
         return this.lista;
     }
     AgregarNota(nota){
-        this.lista.push(nota)
+        this.lista.push(nota);
     }
+    BorrarNota(index){
+        this.lista.splice(index,1);
+        this.MostrarLista();
+    }
+    
+    MostrarLista(){
+        listaNotasHTML.innerText="";
+        this.lista.forEach((nota, index) => {
+            const listItem = document.createElement("li");
+            const title = document.createElement("h3");
+            const content = document.createElement("p");
+            const deleteButton=document.createElement("button");
+            console.log(index);
+            title.innerText = nota.GetTitulo();
+            content.innerText = nota.GetDescripcion();
+            deleteButton.innerText="Eliminar";
+    
+            deleteButton.onclick=()=> {
+                this.BorrarNota(index);
+            };
+            listItem.appendChild(title);
+            listItem.appendChild(content);
+            listItem.appendChild(deleteButton);
+            listaNotasHTML.appendChild(listItem);
+        });
+    }
+    
 }
 export default ListaNotas;
--- src/presenter.js
+++ src/presenter.js
@@ -14,18 +14,5 @@ formularioNota.addEventListener("submit", event => {
     let notaNueva=new Nota(tituloNota.value,descrpcionNota.value);
     listaNotasNueva.AgregarNota(notaNueva);
     listaNotasHTML.innerHTML = "";
-    listaNotasNueva.GetLista().forEach((nota) => {
-        const listItem = document.createElement("li");
-        const title = document.createElement("h3");
-        const content = document.createElement("p");
-    
-        title.innerText = nota.GetTitulo();
-        content.innerText = nota.GetDescripcion();
-    
-        listItem.appendChild(title);
-        listItem.appendChild(content);
-        listaNotasHTML.appendChild(listItem);
-    });
-    contenidoTituloNota.innerHTML = "<p>" + "Título introducido: " + notaNueva.GetTitulo() + "</p>";
-    contenidoDescripcionNota.innerHTML="<p>"+"Descripción Introducida: "+notaNueva.GetDescripcion()+"</p>";
+    listaNotasNueva.MostrarLista();
 })
